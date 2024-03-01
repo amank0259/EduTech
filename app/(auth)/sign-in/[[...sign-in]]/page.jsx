1e457ab141d40faab74eed6cadc874e0956374df
Aman kumar <amank0259@gmail.com>
2024-03-01 18:54:14 +0530

    added user Authentication

--- app/(auth)/sign-in/[[...sign-in]]/page.jsx
+++ app/(auth)/sign-in/[[...sign-in]]/page.jsx
@@ -32,7 +32,7 @@ export default function Page() {
                             </a>
 
                             <h2 className="mt-6 text-2xl font-bold text-white sm:text-3xl md:text-4xl">
-                                Welcome to Squid 🦑
+                                Welcome to EduTech 📖
                             </h2>
 
                             <p className="mt-4 leading-relaxed text-white/90">
