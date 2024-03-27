7f29fd8cf12a0dbf532ee6f659c4d6bf0aa2dc38
Aman kumar <amank0259@gmail.com>
2024-03-27 13:06:26 +0530

    fix page height

--- app/(router)/isntructor/page.jsx
+++ app/(router)/isntructor/page.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function InstructorPage() {
     return (
-        <div className='flex h-screen items-center justify-center'>
+        <div className='flex h-[90vh] items-center justify-center'>
             <h2 className='text-6xl font-bold'>Page Under Construction 🚧</h2>
         </div>
     )
