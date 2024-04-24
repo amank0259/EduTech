c47b71a4d05edb4610d08f21afcc47443f599887
Aman kumar <amank0259@gmail.com>
2024-04-24 17:50:03 +0530

    add newsletter page

---
+++ app/(router)/newsletter/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function page() {
+    return (
+        <div className='flex h-[90vh] items-center justify-center'>
+            <h2 className='text-6xl font-bold'>Page Under Construction 🚧</h2>
+        </div>
+    )
+}
+
+export default page
